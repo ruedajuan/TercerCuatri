e71d593ad759e5f813ac5cefdef5c569d37fd156
Marcelo Latyn Schiavon <111091315+LatynM@users.noreply.github.com>
2023-04-14 11:58:02 -0300

    Add files via upload

---
+++ Programacion/tirar_Dado/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace tirar_Dado
+{
+    class dado
+    {
+        public int numero;
+
+        public dado() {
+            Random rnd = new Random();
+            this.numero = rnd.Next(1, 7);
+        }
+
+        public void tirar_Dado()
+        {
+            Console.WriteLine("Ingrese numero:");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dado: "+this.numero);
+
+            if(this.numero == num)
+            {
+                Console.WriteLine("Acertado");
+            }
+            else
+            {
+                Console.WriteLine("No acertado");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dado d1 = new dado();
+
+            d1.tirar_Dado();
+        }
+    }
+}
